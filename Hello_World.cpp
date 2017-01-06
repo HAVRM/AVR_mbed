5f5f1dec4ffccc8975cd11bf0413d46b067d7dde
HAVRM <hiroki.mine.0614.163@gmail.com>
2017-01-06 17:30:54 +0900

    test

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -1,9 +1,14 @@
 #include "mbed.h"
 
 Serial gps;
+DigitalOut led(PB_0);
 
-#include "gps-sd.h"
+#include "gps.h"
 
 int main(void){
-
+  int time[3],date[3],sats;
+  float east,north,speed,high,dire;
+  while(1){
+    if(get_gps(time,date,&sats,&east,&north,&speed,&high,&dire)==1)led=!led;
+  }
 }
