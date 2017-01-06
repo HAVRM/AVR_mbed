db71697276555db9fb64154ab02ddb9b18f23048
HAVRM <hiroki.mine.0614.163@gmail.com>
2017-01-06 17:49:01 +0900

    test

---
+++ include/gps.h
@@ -0,0 +1,175 @@
+//  Author: hiroki.mine
+
+#ifndef GPS_H_20161224_
+#define GPS_H_20161224_
+#include "serial.h"
+#include "mbed.h"
+
+long ten_x(int a){ //10^a
+  int ans=1;
+  for(int i=0;i<a;i++)ans*=10;
+  return ans;
+}
+
+char get_char_from_nmea(char *data,int num){
+  int read=-1;
+  for(int i=0;i<num;i++){
+    read++;
+    for(int j=0;data[read]!=',';j++){
+      if(data[j]=='\0')return 0;
+      read++;
+    }
+  }
+  return data[read+1];
+}
+
+int get_int_from_nmea(char *data,int num){
+  int ans=0,read=-1;
+  for(int i=0;i<num;i++){
+    read++;
+    for(int j=0;data[read]!=',';j++){
+      if(data[read]=='\0')return 0;
+      read++;
+    }
+  }
+  read++;
+  for(int i=0;data[read+i]!=',';i++){
+    if(data[i+read]=='\0')return ans;
+    ans=ans*10+(data[i+read]-48);
+  }
+  return ans;
+}
+
+long get_long_from_nmea(char *data,int num){
+  long ans=0;
+  int read=-1;
+  for(int i=0;i<num;i++){
+    read++;
+    for(int j=0;data[read]!=',';j++){
+      if(data[read]=='\0')return 0;
+      read++;
+    }
+  }
+  read++;
+  for(int i=0;data[read+i]!=',';i++){
+    if(data[i+read]=='\0')return ans;
+    ans=ans*10+(data[i+read]-48);
+  }
+  return ans;
+}
+
+float get_float_from_nmea(char *data,int num){
+  float ans=0;
+  int read=-1;
+  for(int i=0;i<num;i++){
+    read++;
+    for(int j=0;data[read]!=',';j++){
+      if(data[read]=='\0')return 0;
+      read++;
+    }
+  }
+  read++;
+  for(int i=0;data[read]!='.';i++){
+    if(data[read]=='\0')return ans;
+    ans=ans*10+(data[read]-48);
+    read++;
+  }
+  read++;
+  for(int i=0;data[read+i]!=',';i++){
+    if(data[i+read]=='\0')return ans;
+    ans=ans+(data[i+read]-48)/((float)(ten_x(i+1)));
+  }
+  return ans;
+}
+
+void get_now(int date[3],int time[3]){
+  char c;
+  int ch=0;
+  char temp[50];
+  while(1){
+    c=USART_getc();
+    while(c!='$')c=USART_getc();
+    if(USART_getc()=='G')if(USART_getc()=='P')if(USART_getc()=='Z')if(USART_getc()=='D')if(USART_getc()=='A')if(USART_getc()==','){
+      for(int i=0;i<50;i++)temp[i]=USART_getc();
+      ch=1;
+    }
+    if(ch!=0)break;
+  }
+  time[0]=((long)(get_float_from_nmea(temp,0)))/10000;
+  time[1]=((long)(get_float_from_nmea(temp,0)))/100-time[0]*100;
+  time[2]=((long)(get_float_from_nmea(temp,0)))-time[0]*10000-time[1]*100;
+  date[2]=get_int_from_nmea(temp,1);
+  date[1]=get_int_from_nmea(temp,2);
+  date[0]=get_int_from_nmea(temp,3);
+  date[0]=date[0]%100;
+} 
+
+int get_gps(int time[3],int date[3],int *sats,float *east,float *north,float *high,float *speed,float *dire){ //get->return 0
+  int i,temp;
+  char c;
+  char gga[100], vtg[100], zda[100], gll;
+  int n=0,k=0; //n is numbers of message, k is numbers in messege
+  int get_id=0;
+  int ch=0;
+  while(ch!=0b1111){
+    c=USART_getc();
+    while(c!='$')c=USART_getc();
+    if((c=USART_getc())=='G')if((c=USART_getc())=='P'){
+      if((c=USART_getc())=='G'){
+        if((c=USART_getc())=='G'){
+          if((c=USART_getc())=='A')if((c=USART_getc())==','){
+            c=USART_getc();
+            for(i=0; c!='*' && c!='\n' && i<100;i++){
+              gga[i]=c;
+              c=USART_getc();
+            }
+            ch|=1;
+          }
+        }
+        else if(c=='L')if((c=USART_getc())=='L')if((c=USART_getc())==','){
+          c=USART_getc();
+          for(i=0; c!='*' && c!='\n' && i<100;i++){
+            gll=c;
+            c=USART_getc();
+          }
+          ch|=2;
+        }
+      }
+      else if(c=='V'){
+        if((c=USART_getc())=='T')if((c=USART_getc())=='G')if((c=USART_getc())==','){
+         c=USART_getc();
+         for(i=0; c!='*' && c!='\n' && i<100;i++){
+            vtg[i]=c;
+            c=USART_getc();
+          }
+          ch|=4;
+        }
+      }
+      else if(c=='Z')if((c=USART_getc())=='D')if((c=USART_getc())=='A')if((c=USART_getc())==','){
+        c=USART_getc();
+        for(i=0; c!='*' && c!='\n' && i<100;i++){
+          zda[i]=c;
+          c=USART_getc();
+        }
+        ch|=8;
+      }
+    }
+  }
+  time[0]=((long)(get_float_from_nmea(zda,0)))/10000;
+  time[1]=((long)(get_float_from_nmea(zda,0)))/100-time[0]*100;
+  time[2]=((long)(get_float_from_nmea(zda,0)))-time[0]*10000-time[1]*100;
+  date[2]=get_int_from_nmea(zda,1);
+  date[1]=get_int_from_nmea(zda,2);
+  date[0]=get_int_from_nmea(zda,3);
+  date[0]=date[0]%100;
+  *sats=get_int_from_nmea(gga,6);
+  *east=get_float_from_nmea(gga,3)/((float)(100));
+  *north=get_float_from_nmea(gga,1)/((float)(100));
+  *high=get_float_from_nmea(gga,8);
+  *speed=get_float_from_nmea(vtg,7);
+  *dire=get_float_from_nmea(vtg,0);
+  if(gll!='A')return -1;
+  return 0;
+}
+
+#endif
--- mbed/target/atmega328p/Serial.h
+++ mbed/target/atmega328p/Serial.h
@@ -1,4 +1,5 @@
 // Author: hiroki.mine
+// This is not for Atmega88
 
 
 
@@ -6,13 +7,26 @@
 #define SERIAL_H_20150516_2043_
 
 class Serial{
-	private:
+private:
 	
 	PinName tx;
 	PinName rx;
 	int baudrate,rate,bits,stop_bits;
+	int keta[5];
 	
-	public:
+	void reset_keta(){
+		keta[0]=0;
+		keta[1]=0;
+		keta[2]=0;
+		keta[3]=-1;
+		keta[4]=-1;
+	}
+
+public:
+	
+	void print_keta(){
+		Serial::printf("%d,%d,%d,%d,%d\n\r",Serial::keta[0],Serial::keta[1],Serial::keta[2],Serial::keta[3],Serial::keta[4]);
+	}
 	
 	enum Parity {
 		None = 0,
@@ -34,6 +48,7 @@ class Serial{
 		DDR(nrx,0);
 		Serial::baud();
 		Serial::format();
+		Serial::reset_keta();
 	}
 	
 	void baud(unsigned int nbr=9600,Parity parity=Serial::None,int nst=1){
@@ -50,14 +65,65 @@ class Serial{
 		Serial::bits=nbit;
 		Serial::parity=npr;
 		Serial::stop_bits=nst;
-		UCSR0A=1<<U2X0;
-		UCSR0B=(1<<RXEN0)|(1<<TXEN0);
-		UCSR0C=(npr<<UPM00)|((nst-1)<<USBS0)|(3<<UCSZ00);
+		UCSR0A=UCSR0A|(1<<1);
+		UCSR0B=UCSR0B|(1<<RXEN0)|(1<<TXEN0);
+		UCSR0C=UCSR0C|(npr<<UPM00)|(nst<<USBS0)|((8-5)<<UCSZ00);		
 	}
 	
-	unsigned char getc(){
+	char getc(){
 		while(!UCSR0A&(1<<RXC0));
 		return UDR0;
+	}
+	
+	void scanf(const char *fmt, ...){
+		int *seriali;
+		unsigned int *serialui;
+		long *seriall;
+		unsigned long *serialul;
+		float *serialf;
+		double *seriald;
+		va_list serialarg;
+		va_start(serialarg,fmt);
+		char serialc=*fmt;
+		while(serialc!='\0'){
+			if(serialc!='%')while(Serial::getc()!=serialc);
+			else{
+				fmt++;
+				serialc=*fmt;
+				switch(serialc){
+					case '%':
+						while(Serial::getc()!='%');
+						break;
+					
+					case 'd':
+						seriali=va_arg(serialarg,int*);
+						*seriali=0;
+						serialc=Serial::getc();
+						while((int)serialc>=48 && (int)serialc<=57)*seriali=*seriali*10+(int)serialc-48;
+						break;
+					
+					case 'f':
+						int state=0;
+						serialf=va_arg(serialarg,float*);
+						*serialf=0;
+						serialc=Serial::getc();
+						while(1){
+							if((int)serialc>=48 && (int)serialc<=57){
+								if(state==0)*serialf=*serialf*10+(int)serialc-48;
+								else{
+									*serialf=*serialf+((int)serialc-48)/((float)(10^state));
+									state++;
+								}								
+							}else if(state==0 && serialc=='.'){
+								state=1;
+							}else break;
+						}
+						
+				}
+			}
+			fmt++;
+			serialc=*fmt;
+		}
 	}
 	
 	int readable(){
@@ -70,37 +136,154 @@ class Serial{
 		_delay_ms(1.5);
 	}
 	
-	void putn(long num){
+	void puts(char *str){
+		char *t_str=str;
+		if(keta[3]!=-1){
+			keta[4]=0;
+			while(*t_str!='\0'){
+				keta[4]++;
+				t_str++;
+			}		
+			if(keta[1]==0){
+				while(keta[3]>keta[4]){
+					Serial::putc(' ');
+					keta[3]--;
+				}				
+			}
+		}
+		while(*str!='\0'){
+			Serial::putc(*str);
+			str++;
+		}
+		if(keta[1]==1){
+			while(keta[3]>keta[4]){
+				Serial::putc(' ');
+				keta[3]--;
+			}
+		}		
+	}
+	
+	void putn(long num,int n=10,int miner=0){
 		char l[100];
-		int i=0;
 		if(num<0){
 			num*=-1;
-			Serial::putc('-');
+			miner=1;
+		}		
+		keta[4]=log10(num)/log10(n);
+		keta[4]++;
+		int i=0;
+		if(keta[0]==1 || miner==1)keta[4]++;
+		if(keta[1]==0 && keta[3]!=-1){
+			while(keta[4]<keta[3]){
+				if(keta[2]==0)Serial::putc(' ');
+				else Serial::putc('0');
+				keta[3]--;
+			}
 		}
-		while(num>0){
-			l[i]=num%10+48;
-			num/=10;
-			i++;
+		if(miner==1)Serial::putc('-');
+		else if(keta[0]==1)Serial::putc('+');
+		if(num==0)Serial::putc('0');
+		else{
+			while(num>0){
+				l[i]=num%n+48;
+				num/=n;
+				if(l[i]>57){
+					switch(l[i]-48){
+						case 10:
+							l[i]='a';
+							break;
+						
+						case 11:
+							l[i]='b';
+							break;
+						
+						case 12:
+							l[i]='c';
+							break;
+						
+						case 13:
+							l[i]='d';
+							break;
+						
+						case 14:
+							l[i]='e';
+							break;
+						
+						case 15:
+							l[i]='f';
+							break;
+						
+						default:
+							break;
+					}
+				}
+				i++;
+			}
+			for(int j=i-1;j>=0;j--)Serial::putc(l[j]);
 		}
-		for(int j=i-1;j>=0;j--)Serial::putc(l[j]);
+		if(keta[1]==1 && keta[3]!=-1){
+			while(keta[4]<keta[3]){
+				Serial::putc(' ');
+				keta[3]--;
+			}
+		}			
 	}
 	
 	void putlf(double num){
+		int t_k[5],miner=0;
+		t_k[0]=keta[0];
+		t_k[1]=keta[1];
+		t_k[2]=keta[2];
+		t_k[3]=keta[3];
+		t_k[4]=keta[4];
+		if(num<0){
+			miner=1;
+			num*=-1;
+		}
 		long h=(long)num;
 		long l;
 		num-=h;
-		while((double)l!=num){
-			num*=10;
-			l=(long)num;
+		if(t_k[3]!=-1 && t_k[4]==-1){
+			t_k[4]=t_k[3]-(int)log10(h)-1-(miner | t_k[0]);
+			keta[3]-=t_k[4];
 		}
-		Serial::putn(h);
+		if(t_k[4]!=-1){
+			for(int i=0;i<t_k[4];i++)num*=10;
+			l=num;
+		}else{
+			t_k[4]=0;
+			while((double)l!=num){
+				num*=10;
+				l=(long)num;
+				t_k[4]++;
+			}
+		}
+		if(keta[1]==1)keta[3]=-1;
+		else if(keta[3]!=-1 && keta[4]!=-1)keta[3]-=t_k[4];
+		//else if(keta[3]!=-1)
+		Serial::putn(h,10,miner);
 		Serial::putc('.');
+		//Serial::print_keta();
+		t_k[0]=0;
+		if(t_k[1]==0)t_k[3]=t_k[4];
+		else if(t_k[3]!=-1)t_k[3]-=keta[4];
+		keta[0]=t_k[0];
+		keta[1]=t_k[1];
+		keta[2]=t_k[2];
+		keta[3]=t_k[3];
+		keta[4]=t_k[4];
 		Serial::putn(l);
 	}
 	
 	void printf(const char *fmt, ...){
+		int k_n=0;
 		int seriali;
+		unsigned int serialui;
+		long seriall;
+		unsigned long serialul;
 		float serialf;
+		double seriald;
+		char *serials;
 		va_list serialarg;
 		va_start(serialarg,fmt);
 		char serialc=*fmt;
@@ -109,26 +292,133 @@ class Serial{
 				Serial::putc(serialc);
 				fmt++;
 			}else{
+				Serial::reset_keta();
+				k_n=0;
+rereade:
 				fmt++;
 				serialc=*fmt;
 				switch(serialc){
 					
 					case '%':
-					Serial::putc('%');
-					break;
+						Serial::putc('%');
+						break;
+					
+					case 'b':
+						seriali=va_arg(serialarg,int);
+						Serial::putn((long)seriali,2);
+						break;
 					
 					case 'd':
-					seriali=va_arg(serialarg,int);
-					Serial::putn((long)seriali);
-					break;
-					
+						seriali=va_arg(serialarg,int);
+						Serial::putn((long)seriali);
+						break;
+				
 					case 'f':
-					serialf=va_arg(serialarg,double);
-					Serial::putlf(serialf);
-					break;
+						serialf=va_arg(serialarg,double);
+						Serial::putlf(serialf);
+						break;
 					
+					case 'l':
+						fmt++;
+						serialc=*fmt;
+						switch(serialc){
+							
+							case 'b':
+								seriali=va_arg(serialarg,long);
+								Serial::putn(seriali,2);
+								break;
+							
+							case 'd':
+								seriall=va_arg(serialarg,long);
+								Serial::putn(seriall);
+								break;
+						
+							case 'f':
+								seriald=va_arg(serialarg,double);
+								Serial::putlf(seriald);
+								break;
+						
+							case 'o':
+								seriall=va_arg(serialarg,long);
+								Serial::putn(seriall,8);
+								break;
+						
+							case 'u':
+								serialul=va_arg(serialarg,unsigned long);
+								Serial::putn((long)serialul);
+								break;
+						
+							case 'x':
+								seriall=va_arg(serialarg,long);
+								Serial::putn(seriall,16);
+								break;
+							
+							default:
+								break;
+						}
+						break;
+					
+					case 'o':
+						seriali=va_arg(serialarg,int);
+						Serial::putn((long)seriali,8);
+						break;
+					
+					case 's':
+						serials=va_arg(serialarg,char*);
+						Serial::puts(serials);
+						break;
+					
+					case 'u':
+						serialui=va_arg(serialarg,unsigned int);
+						Serial::putn((long)serialui);
+						break;
+					
+					case 'x':
+						seriali=va_arg(serialarg,int);
+						Serial::putn((long)seriali,16);
+						break;
+					
+					case '0':
+						if(k_n<=2){
+							k_n=3;
+							keta[2]=1;
+						}else{
+							k_n=4;
+							keta[4]=0;
+						}
+						goto rereade;
+					
+					case '-':
+						if(k_n<=1){
+							k_n=2;
+							keta[1]=1;
+						}
+						goto rereade;
+					case '+':
+						if(k_n==0){
+							k_n=1;
+							keta[0]=1;
+						}
+						goto rereade;
+					
+					case '.':
+						k_n=4;
+						goto rereade;
+					
+					default:
+						if(48<serialc && serialc<=57){
+							if(k_n<=3){
+								k_n=3;
+								if(keta[3]==-1)keta[3]=serialc-48;
+								else keta[3]=keta[3]*10+serialc-48;
+							}else if(k_n==4){
+								if(keta[4]==-1)keta[4]=serialc-48;
+								else keta[4]=keta[4]*10+serialc-48;
+							}
+						}
+						goto rereade;											
 				}
-				fmt++;
+				fmt++;				
 			}
 		}
 	}
